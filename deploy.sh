60183b296b51e36239cc81887c74fd8787ce72d0
axiang <wyxworkmail@163.com>
2020-06-16 12:42:26 +0800

    build: 自动化部署

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# 以支持全自动部署
+npm install
+npm run build
+sudo docker pull nginx
+sudo docker stop coding_first_manager_web_cli
+sudo docker rm coding_first_manager_web_cli
+sudo docker rmi coding_first_manager_web_cli
+sudo docker build -t coding_first_manager_web_cli .
+sudo docker run -dp 8882:80 -d --name coding_first_manager_web_cli coding_first_manager_web_cli
+
+
+
+
+
