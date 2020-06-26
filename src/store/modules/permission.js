93a380a55bcac393f51604de2dd07f0bc99377ff
axiang <wyxworkmail@163.com>
2020-06-26 22:08:21 +0800

    fix: 修复部署后刷新状态不保存问题

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,69 +1,71 @@
-import { asyncRoutes, constantRoutes } from '@/router'
+import router from './router'
+import store from './store'
+import { Message } from 'element-ui'
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+import { getToken } from '@/utils/auth' // get token from cookie
+import getPageTitle from '@/utils/get-page-title'
 
-/**
- * 通过meta中的role属性来决定当前用户是否拥有权限
- * @param roles
- * @param route
- */
-function hasPermission(roles, route) {
-  if (route.meta && route.meta.roles) {
-    return roles.some(role => route.meta.roles.includes(role))
-  } else {
-    return true
-  }
-}
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
-/**
- * Filter asynchronous routing tables by recursion
- * @param routes asyncRoutes
- * @param roles
- */
-export function filterAsyncRoutes(routes, roles) {
-  const res = []
+const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
 
-  routes.forEach(route => {
-    const tmp = { ...route }
-    if (hasPermission(roles, tmp)) {
-      if (tmp.children) {
-        tmp.children = filterAsyncRoutes(tmp.children, roles)
-      }
-      res.push(tmp)
-    }
-  })
+router.beforeEach(async(to, from, next) => {
+  // start progress bar
+  NProgress.start()
 
-  return res
-}
+  // set page title
+  document.title = getPageTitle(to.meta.title)
 
-const state = {
-  routes: [],
-  addRoutes: []
-}
+  // determine whether the user has logged in
+  const hasToken = getToken()
 
-const mutations = {
-  setRoutes: (state, routes) => {
-    state.addRoutes = routes
-    state.routes = constantRoutes.concat(routes)
-  }
-}
-
-const actions = {
-  generateRoutes({ commit }, roles) {
-    return new Promise(resolve => {
-      let accessedRoutes
-      if (roles.includes('superAdmin')) {
-        accessedRoutes = asyncRoutes || []
+  if (hasToken) {
+    if (to.path === '/login') {
+      // if is logged in, redirect to the home page
+      next({ path: '/' })
+      NProgress.done()
+    } else {
+      // determine whether the user has obtained his permission roles through getInfo
+      const hasRoles = store.getters.roles && store.getters.roles.length > 0
+      if (hasRoles) {
+        next()
       } else {
-        accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
-      }
-      commit('setRoutes', accessedRoutes)
-      resolve(accessedRoutes)
-    })
-  }
-}
+        try {
+          const roles = await store.dispatch('user/getInfo')
+          // generate accessible routes map based on roles
+          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
 
-export default {
-  namespaced: true,
-  state,
-  mutations,
-  actions
-}
+          // dynamically add accessible routes
+          router.addRoutes(accessRoutes)
+
+          // hack method to ensure that addRoutes is complete
+          // set the replace: true, so the navigation will not leave a history record
+          next({ ...to, replace: true })
+        } catch (error) {
+          // remove token and go to login page to re-login
+          await store.dispatch('user/resetToken')
+          Message.error(error || 'Has Error')
+          next(`/login?redirect=${to.path}`)
+          NProgress.done()
+        }
+      }
+    }
+  } else {
+    /* has no token*/
+
+    if (whiteList.indexOf(to.path) !== -1) {
+      // in the free login whitelist, go directly
+      next()
+    } else {
+      // other pages that do not have permission to access are redirected to the login page.
+      next(`/login?redirect=${to.path}`)
+      NProgress.done()
+    }
+  }
+})
+
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done()
+})
